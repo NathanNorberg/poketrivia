4324cfe9eb5dd209e7e971c4a2c8369f0c456275
Marko Pecic <mpecic2010@gmail.com>
2018-10-09 14:50:35 -0700

    pokedex works

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -1,14 +1,17 @@
 const pokedexURL = "https://pokeapi.co/api/v2/pokemon/";
 
 const pdxEntry = document.querySelector('.pdxEntry');
-let newDiv = document.createElement("div");
-let newP = document.createElement("P");
+// let newDiv = document.createElement("div");
+let newDiv;
 
 axios.get(pokedexURL).then(res => {
-  // for(let i =0; i< 151; i++){
-  console.log(res.data.results[0].name);
-  newDiv.setAttribute("class","col");
-  newP.innerHTML = res.data.results[0].name;
-  pdxEntry.appendChild(newDiv.appendChild(newP));
-  // }
+  for(let i =0; i< 151; i++){
+  // console.log(res.data.results[i].name);
+  newDiv = document.createElement('div')
+  let newP = document.createElement("P");
+  newDiv.setAttribute("class","row border border-dark");
+  newDiv.appendChild(newP);
+  newP.innerHTML = res.data.results[i].name;
+  pdxEntry.appendChild(newDiv);
+  }
 });
